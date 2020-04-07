ed797808329197267f49c5a760381306679130cb
ChanPark <thechanmoon@gmail.com>
2020-04-07 08:19:06 -0400

    Done.

---
+++ src/Adapter.js
@@ -0,0 +1,20 @@
+class Adapter{
+
+    static getDogs()
+    {
+        return fetch("http://localhost:3000/pups")
+        .then(res => res.json())
+    }
+
+    static updateDog(dog){
+        const options = {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({isGoodDog: dog.isGoodDog})
+          }
+          return fetch(`http://localhost:3000/pups/${dog.id}`, options)
+            .then(res => res.json())
+    }
+}
